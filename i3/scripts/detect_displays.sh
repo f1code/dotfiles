1631b593daae78bf960212e0a95782c0e55c4ffd
Nicolas Galler <ngaller@gmail.com>
2017-03-23 09:05:33 -0400

    Detect displays - setup for laptop

--- i3/scripts/detect_displays.sh
+++ i3/scripts/detect_displays.sh
@@ -1,79 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# from https://wiki.archlinux.org/index.php/xrandr#Scripts
-# TODO: figure out how to use srandr to do this automatically when 
-# display is plugged / unplugged
-
-XRANDR="xrandr"
-CMD="${XRANDR}"
-declare -A VOUTS
-eval VOUTS=$(${XRANDR}|awk 'BEGIN {printf("(")} /^\S.*connected/{printf("[%s]=%s ", $1, $2)} END{printf(")")}')
-declare -A POS
-#XPOS=0
-#YPOS=0
-#POS="${XPOS}x${YPOS}"
-
-POS=([X]=0 [Y]=0)
-
-find_mode() {
-  echo $(${XRANDR} |grep ${1} -A1|awk '{FS="[ x]"} /^\s/{printf("WIDTH=%s\nHEIGHT=%s", $4,$5)}')
-}
-
-xrandr_params_for() {
-  if [ "${2}" == 'connected' ]
-  then
-    eval $(find_mode ${1})  #sets ${WIDTH} and ${HEIGHT}
-    MODE="${WIDTH}x${HEIGHT}"
-    CMD="${CMD} --output ${1} --mode ${MODE} --pos ${POS[X]}x${POS[Y]}"
-    POS[X]=$((${POS[X]}+${WIDTH}))
-    return 0
-  else
-    CMD="${CMD} --output ${1} --off"
-    return 1
-  fi
-}
-
-for VOUT in ${!VOUTS[*]}
-do
-  xrandr_params_for ${VOUT} ${VOUTS[${VOUT}]}
-done
-set -x
-${CMD}
-set +x
-
-XRANDR="xrandr"
-CMD="${XRANDR}"
-declare -A VOUTS
-eval VOUTS=$(${XRANDR}|awk 'BEGIN {printf("(")} /^\S.*connected/{printf("[%s]=%s ", $1, $2)} END{printf(")")}')
-declare -A POS
-#XPOS=0
-#YPOS=0
-#POS="${XPOS}x${YPOS}"
-
-POS=([X]=0 [Y]=0)
-
-find_mode() {
-  echo $(${XRANDR} |grep ${1} -A1|awk '{FS="[ x]"} /^\s/{printf("WIDTH=%s\nHEIGHT=%s", $4,$5)}')
-}
-
-xrandr_params_for() {
-  if [ "${2}" == 'connected' ]
-  then
-    eval $(find_mode ${1})  #sets ${WIDTH} and ${HEIGHT}
-    MODE="${WIDTH}x${HEIGHT}"
-    CMD="${CMD} --output ${1} --mode ${MODE} --pos ${POS[X]}x${POS[Y]}"
-    POS[X]=$((${POS[X]}+${WIDTH}))
-    return 0
-  else
-    CMD="${CMD} --output ${1} --off"
-    return 1
-  fi
-}
-
-for VOUT in ${!VOUTS[*]}
-do
-  xrandr_params_for ${VOUT} ${VOUTS[${VOUT}]}
-done
-set -x
-${CMD}
-set +x
+# setup dual monitor with primary on HDMI
+if xrandr --query | grep HDMI1 | grep disconnected; then 
+    xrandr --output eDP1 --auto --primary 
+else
+    xrandr --output eDP1 --off --output HDMI1 --auto --primary --output VGA1 --auto --right-of HDMI1
+    #xrandr --output HDMI1 --auto --primary --output VGA1 --auto --right-of HDMI1
+fi
