9fb44f397fe24cfe1fffc2ce85ba85f1d234106a
Nicolas Galler <ngaller@gmail.com>
2017-03-23 09:23:49 -0400

    Tweaks for display detect

--- i3/scripts/detect_displays.sh
+++ i3/scripts/detect_displays.sh
@@ -41,24 +41,16 @@ done <<< "$DEVICES"
 if [ ! -z "$HDMI1" -a ! -z "$VGA1" ]
 then
   echo "HDMI1 and VGA1 are plugged in"
-  xrandr --output eDP1 --off
-  xrandr --output HDMI1 --auto --primary
-  xrandr --output VGA1 --auto --right-of HDMI1 --noprimary
+  xrandr --output eDP1 --off --output HDMI1 --auto --primary --output VGA1 --auto --right-of HDMI1 
 elif [ ! -z "$HDMI1" -a -z "$VGA1" ]; then
   echo "HDMI1 is plugged in, but not VGA1"
-  xrandr --output eDP1 --off
-  xrandr --output VGA1 --off
-  xrandr --output HDMI1 --auto --primary
+  xrandr --output VGA1 --off --output eDP1 --off --output HDMI1 --auto --primary
 elif [ -z "$HDMI1" -a ! -z "$VGA1" ]; then
   echo "VGA1 is plugged in, but not HDMI1"
-  xrandr --output eDP1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output VGA1 --auto --primary
+  xrandr --output HDMI1 --off --output eDP1 --off --output VGA1 --auto --primary
 else
   echo "No external monitors are plugged in"
-  xrandr --output eDP1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output LVDS1 --auto --primary
+  xrandr --output HDMI1 --off --output VGA1 --off --output eDP1 --auto --primary
 fi
 
 # if xrandr --query | grep HDMI1 | grep disconnected; then 
