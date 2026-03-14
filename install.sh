fc9bd3f5cdf52022cac004cd203b8a9a40a53d28
Nicolas Galler <ngaller@gmail.com>
2026-03-14 06:28:08 +0100

    Install script fix for linux

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-$DOTFILES=~/.config/dotfiles
+DOTFILES=$HOME/.config/dotfiles
 ln -s $DOTFILES/ghostty ~/.config
 ln -s $DOTFILES/karabiner ~/.config
 ln -s $DOTFILES/zellij ~/.config
-cp $DOTFILES/zshenv ~
+cp $DOTFILES/zshenv ~/.zshenv
 # for i in bin/*; do
 #     ln -sf ~/.dotfiles/$i ~/$i 
 # done
