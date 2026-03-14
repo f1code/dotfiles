eef8816e78665ebe9fedf4fc174bb91c900b22f1
Nicolas Galler <ngaller@gmail.com>
2026-03-14 06:11:28 +0100

    Install script

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 $DOTFILES=~/.config/dotfiles
 ln -s $DOTFILES/ghostty ~/.config
 ln -s $DOTFILES/karabiner ~/.config
-ln -s $DOTFILES/zsh/.zshenv ~
+ln -s $DOTFILES/zellij ~/.config
+cp $DOTFILES/zshenv ~
 # for i in bin/*; do
 #     ln -sf ~/.dotfiles/$i ~/$i 
 # done
