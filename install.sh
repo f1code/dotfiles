41fa29e4da1c0f49a7bf3f37bdba6dd6130e959e
Nicolas Galler <ngaller@gmail.com>
2026-05-15 13:36:07 +0200

    Opencode config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DOTFILES=$HOME/.config/dotfiles
 ln -s $DOTFILES/ghostty ~/.config
 ln -s $DOTFILES/karabiner ~/.config
 ln -s $DOTFILES/zellij ~/.config
+ln -s $DOTFILES/opencode ~/.config
 cp $DOTFILES/zshenv ~/.zshenv
 # for i in bin/*; do
 #     ln -sf ~/.dotfiles/$i ~/$i 
