3f57c2505397b960a34128159921fc46fd713f67
Nicolas Galler <ngaller@gmail.com>
2026-07-05 07:56:59 -0400

    fnm: cleanup multishell on exit

--- zsh/conf.d/30-tools.zsh
+++ zsh/conf.d/30-tools.zsh
@@ -3,6 +3,12 @@ if [ -d ~/.local/share/fnm ]; then
   export FNM_PATH=~/.local/share/fnm
   export PATH=$PATH:$FNM_PATH
 fi
+cleanup_fnm() {
+  if [[ -n "$FNM_MULTISHELL_PATH" && -d "$FNM_MULTISHELL_PATH" ]]; then
+    rm -rf "$FNM_MULTISHELL_PATH"
+  fi
+}
+trap cleanup_fnm EXIT
 eval "$(fnm env --use-on-cd --log-level quiet)"
 
 # Rust
@@ -14,3 +20,5 @@ eval "$(zoxide init zsh --cmd cd)"
 # SCM Puff [https://github.com/mroth/scmpuff]
 eval "$(scmpuff init -s --aliases=false)"
 alias gs="scmpuff_status"
+
+if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
--- zsh/conf.d/40-aliases.zsh
+++ zsh/conf.d/40-aliases.zsh
@@ -13,6 +13,7 @@ alias lni="${aliases[ln]:-ln} -i"
 alias mvi="${aliases[mv]:-mv} -i"
 alias rmi="${aliases[rm]:-rm} -i"
 alias rg='rg -S'
+alias kssh='kitten ssh'
 
 if command -v eza >/dev/null 2>&1; then
   alias ls='eza --icons=always --hyperlink'
